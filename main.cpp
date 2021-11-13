3f2cdba02d64ecafba5fa0ff8e097aed790fc98f
Alessandro Mezzacasa <91669008+mezzaux@users.noreply.github.com>
2021-11-13 12:15:42 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-   return 0;
+    int n=0;
+    cin>>n;
+    int risultato=0,primo=1,secondo=0;
+    while(risultato<n){
+        risultato=primo+secondo;
+        primo=secondo;
+        secondo=risultato;
+        cout<<risultato<<endl;
+        risultato=primo+secondo;
+    }
 }
