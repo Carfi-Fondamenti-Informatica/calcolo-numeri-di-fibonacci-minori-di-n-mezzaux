ed07b0615e248daf7fd4f89aaac347e0c3870002
Alessandro Mezzacasa <91669008+mezzaux@users.noreply.github.com>
2021-11-13 12:19:14 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ int main() {
     int n=0;
     cin>>n;
     int risultato=0,primo=1,secondo=0;
-    while(risultato<n){
+    while(risultato<=n){
         risultato=primo+secondo;
         primo=secondo;
         secondo=risultato;
